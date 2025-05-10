5b0f7ee0923880e727b88a2293ea8709b17ec2b5
Hacker-8851 <kumarsachin8851311@gmail.com>
2025-05-10 20:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+    let totalCost = 0;
 
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b); // Sort ascending
+
+        // Take the two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        // Add the combined rope back
+        arr.push(cost);
+    }
+
+    return totalCost;
+}
 module.exports=mincost;
